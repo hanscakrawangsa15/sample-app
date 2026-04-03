11e749ab1d405824fb187620a1ddb5f3d6f30957
Sample User <sample@example.com>
2026-04-03 12:47:02 +0000

    edit .sh file

--- sample-app.sh
+++ sample-app.sh
@@ -8,8 +8,8 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "FROM python:3.10" > tempdir/Dockerfile
+echo "RUN pip install flask --progress-bar off" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
